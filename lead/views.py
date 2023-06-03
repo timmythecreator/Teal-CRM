95e7a0ccfeffdf71c2a7c37b5504f4a834d1280e
Timur <androsov.tg@gmail.com>
2023-06-04 01:54:55 +0200

    Added lead app

---
+++ lead/views.py
@@ -0,0 +1,77 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import AddLeadForm
+from .models import Lead
+
+
+@login_required
+def leads_list(request):
+    leads = Lead.objects.filter(created_by=request.user)
+
+    return render(request, 'lead/leads_list.html', {
+        'leads': leads
+    })
+
+
+@login_required
+def leads_detail(request, pk):
+    # making sure that I'll get only that Leads I created by my own
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+    # lead = Lead.objects.filter(created_by=request.user).get(pk=pk)
+
+    return render(request, 'lead/leads_detail.html', {
+        'lead': lead
+    })
+
+
+@login_required
+def leads_delete(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+    lead.delete()
+
+    messages.success(request, 'The lead was deleted.')
+
+    return redirect('leads_list')
+
+
+@login_required
+def leads_edit(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST, instance=lead)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'The changes were saved.')
+
+            return redirect('leads_list')
+    else:
+        form = AddLeadForm(instance=lead)
+
+    return render(request, 'lead/leads_edit.html', {
+        'form': form
+    })
+
+
+@login_required
+def add_lead(request):
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST)
+
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+
+            messages.success(request, 'The lead was added.')
+
+            return redirect('leads_list')
+    else:
+        form = AddLeadForm()
+
+    return render(request, 'lead/add_lead.html', {
+        'form': form
+    })
